4ba1dad825d303706dc5c1a334010c5eb0bd8ebf
IhorKozloff <ik.eyeshield21@gmail.com>
2022-01-01 12:53:15 +0200

    promises done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,47 +1,70 @@
+import Notiflix from 'notiflix';
+
 const refs = {
   firstDelayEl: document.querySelector('[name="delay"]'),
   delayStepEl: document.querySelector('[name="step"]'),
   amountEl: document.querySelector('[name="amount"]'),
   submitBtnEl: document.querySelector('button'),
 };
-let index = 1;
 
-console.log(refs.firstDelayEl)
-console.log(refs.delayStepEl)
-console.log(refs.amountEl)
-console.log(refs.submitBtnEl)
+let promisPosition = 1;
+
+function onBtnClick (event) {
+  event.preventDefault();
+
+  setTimeout(() => {
+    createPromise(promisPosition, refs.firstDelayEl.value).then(onResolveMyPromise).catch(onRejectMyPromise);
+    promisPosition += 1;
+    let delayResult = Number(refs.firstDelayEl.value) + Number(refs.delayStepEl.value);
 
 
-function onBtnClick () {
-  intervalId = setInterval(createPromise, refs.delayStepEl.value);
-}
+
+
+    const intIdentity = setInterval(() => {
+
+        if (promisPosition > refs.amountEl.value) {
+          clearInterval(intIdentity);
+        }
+        else {  
+          createPromise(promisPosition, delayResult).then(onResolveMyPromise).catch(onRejectMyPromise);
+          promisPosition += 1;
+          delayResult += Number(refs.delayStepEl.value);
+        };
+      },refs.delayStepEl.value);
+     
+
+  }, refs.firstDelayEl.value)
+};
 
 
 function createPromise(position, delay) {
-  if (index === refs.amountEl.value) {
-    clearInterval(intervalId);
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        
+      } else {
+      
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }  
   }
-  const shouldResolve = Math.random();
- console.log(shouldResolve);
- index += 1 
- console.log(index);
- console.log(refs.amountEl.value)
-  // if (shouldResolve) {
-  //   // Fulfill
-  // } else {
-  //   // Reject
-  // }
-}
+)};
 
-function ifResolveAction (result) {
-  console.log(result);
-};
+function  onResolveMyPromise (resolveResultValue) {
+  Notiflix.Notify.success(resolveResultValue);
+  console.log(resolveResultValue);
+}; 
 
-function ifRejectAction (result) {
-  console.log(result);
-};
-
-// createPromise(3000).then(ifResolveAction);
+function  onRejectMyPromise (rejectResultValue) {
+  Notiflix.Notify.failure(rejectResultValue)
+  console.log(rejectResultValue);
+}; 
 
 
-refs.submitBtnEl.addEventListener('click', onBtnClick)
+
+
+
+
+
+
+refs.submitBtnEl.addEventListener('click', onBtnClick);
